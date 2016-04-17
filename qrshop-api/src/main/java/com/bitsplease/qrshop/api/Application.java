2ee5a2d03d82e6a24683d927235782623dc98d21
Chrisostomos Bakouras <chbakouras@gmail.com>
2016-04-17 14:20:58 +0300

    Update Spring security

--- qrshop-api/src/main/java/com/bitsplease/qrshop/api/Application.java
+++ qrshop-api/src/main/java/com/bitsplease/qrshop/api/Application.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Import;
  */
 @Import({
         com.bitsplease.qrshop.domain.DomainConfig.class,
-//        com.bitsplease.qrshop.security.SecurityConfig.class,
+        com.bitsplease.qrshop.security.SecurityConfig.class,
         com.bitsplease.qrshop.service.ServiceConfig.class,
         com.bitsplease.qrshop.mapper.MapperConfig.class
 })
